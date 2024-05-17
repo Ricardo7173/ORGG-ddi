7a0cbd18a4628f8e15ffa0ddf027f558111e1196
Ricardo7173 <riki0303@outlook.es>
2024-05-16 23:45:06 -0600

    Ejercicios

---
+++ Ejercicios/01_Hello_World.dart
@@ -0,0 +1,14 @@
+//Hello world
+void main () {
+  String name = 'Richo';
+  late final int version;
+  
+  version = 12;
+  const String MyName = 'Richardo';
+  
+  print("Hello $name");
+  print ("Hello ${name.toUpperCase()}!");
+  print("${1+1}");
+  print("No. version: $version");
+  print("Constantes: $MyName");
+}
