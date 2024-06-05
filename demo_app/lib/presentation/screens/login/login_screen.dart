bdd61bdc3caba22713fe3b5f9082517f90912928
Ricardo7173 <riki0303@outlook.es>
2024-06-05 10:09:34 -0600

    Login mejorado

--- demo_app/lib/presentation/screens/login/login_screen.dart
+++ demo_app/lib/presentation/screens/login/login_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
@@ -10,65 +9,90 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   int clickCounter = 0;
+  bool obscurePassword = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text("Login"),
+        title: const Text("Login"),
         leading: IconButton(
           icon: const Icon(Icons.verified_user),
-          onPressed: () { },
+          onPressed: () {},
         ),
         backgroundColor: Colors.blueAccent,
         actions: [
-          IconButton(onPressed: () { }, icon: const Icon(Icons.person_2_sharp)),
-          IconButton(onPressed: () { }, icon: const Icon(Icons.more_vert))
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.person_2_sharp),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+          )
         ],
       ),
-      body:  Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Login', style: const TextStyle(
-              fontSize: 60, 
-              fontWeight: FontWeight.w100
-              )),
-              const SizedBox(height: 30),
-            const Text('Ingresa tu correo',style: TextStyle(fontSize: 25)),
+            const Text(
+              'Login',
+              style: TextStyle(fontSize: 60, fontWeight: FontWeight.w100),
+            ),
+            const SizedBox(height: 30),
+            const Text('Ingresa tu correo', style: TextStyle(fontSize: 25)),
             const SizedBox(height: 30),
             TextFormField(
-              obscureText: true,
+              obscureText: obscurePassword, // Oculta los caracteres de la contraseña
+              decoration: InputDecoration(
+                labelText: 'Contraseña',
+                suffixIcon: IconButton(
+                  icon: Icon(obscurePassword
+                      ? Icons.visibility
+                      : Icons.visibility_off),
+                  onPressed: () {
+                    setState(() {
+                      obscurePassword = !obscurePassword;
+                    });
+                  },
+                ),
+              ),
             ),
           ],
-        ) 
         ),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            CustomButton(
-              icon: Icons.refresh, 
-              onPressed: () {
-              clickCounter = 0;
-              setState(() { });
-            },),
-            const SizedBox(height: 10,),
-            CustomButton(
-              icon: Icons.plus_one,
-              onPressed: () {
-              clickCounter ++;
-              setState(() { });
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          CustomButton(
+            icon: Icons.refresh,
+            onPressed: () {
+              setState(() {
+                clickCounter = 0;
+              });
             },
-              ),
-            const SizedBox(height: 10,),
-            CustomButton(
-              icon: Icons.exposure_minus_1,
-              onPressed: () {
-              clickCounter --;
-              setState(() { });
+          ),
+          const SizedBox(height: 10),
+          CustomButton(
+            icon: Icons.plus_one,
+            onPressed: () {
+              setState(() {
+                clickCounter++;
+              });
             },
-              ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 10),
+          CustomButton(
+            icon: Icons.exposure_minus_1,
+            onPressed: () {
+              setState(() {
+                clickCounter--;
+              });
+            },
+          ),
+        ],
+      ),
     );
   }
 }
@@ -77,16 +101,16 @@ class CustomButton extends StatelessWidget {
   final IconData icon;
   final VoidCallback? onPressed;
   const CustomButton({
-    super.key,
+    Key? key,
     required this.icon,
     required this.onPressed,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
       shape: const StadiumBorder(),
-      onPressed: onPressed, 
+      onPressed: onPressed,
       child: Icon(icon),
     );
   }
